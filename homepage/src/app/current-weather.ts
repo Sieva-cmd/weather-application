a6b3cd0507f84c5aa8ab1ebc58902564c9d2bf4c
Sieva-cmd <musyokasieva@gmail.com>
2022-03-15 16:47:40 +0300

    creating different components, classes and service for our app

---
+++ homepage/src/app/current-weather.ts
@@ -0,0 +1,5 @@
+export class CurrentWeather {
+    constructor(
+        public name:string,public temp:string,public main:string,public temp_min:string,public temp_max:string ,public description:string
+    ){}
+}
